f3a50237b7380fc74ab6886b4e326cc2af00dba2
tylerku <ty.udy22@gmail.com>
2018-03-23 18:07:51 -0600

    First Commit

---
+++ Fortnite Assistant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fortnite Assistant
+//
+//  Created by Ty Udy on 3/23/18.
+//  Copyright © 2018 Ty Udy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
