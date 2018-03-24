8747a02e676d481cda7b1c5e12a4054dd98116bc
tylerku <ty.udy22@gmail.com>
2018-03-23 19:44:42 -0600

    Add new VC

--- Fortnite Assistant/HomeViewController.swift
+++ Fortnite Assistant/HomeViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setup()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setup(){
+        // startButton.layer.backgroundColor = UIColor.white.cgColor
+        startButton.layer.cornerRadius = CGFloat(35)
     }
 
 
---
+++ Fortnite Assistant/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  Fortnite Assistant
+//
+//  Created by Ty Udy on 3/23/18.
+//  Copyright © 2018 Ty Udy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do stuff...
+        
+    }
+}
