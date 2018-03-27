a3b932df05e73f79c634dc2138d08a2b2bf0dd80
tylerku <ty.udy22@gmail.com>
2018-03-27 17:28:49 -0600

    Add MapCalculator Class

---
+++ Fortnite Assistant/Utils/MapCalculator.swift
@@ -0,0 +1,25 @@
+//
+//  MapCalculator.swift
+//  Fortnite Assistant
+//
+//  Created by Ty Udy on 3/27/18.
+//  Copyright © 2018 Ty Udy. All rights reserved.
+//
+
+import Foundation
+
+class MapCalculator {
+    // Access this function from another class with MapCalculator.calculateRoute()
+    static func calculateRoute(){
+        // Note: This function should probably encorporate the Line object that is going to be stored inside the MapView at runtime. Checkout the object in the Models folder. Idk if this function will even be necessary because of that? Buy maybe it will be? 
+        //...
+    }
+    
+    static func calculateDistance(){
+        //...
+    }
+    
+    static func scoreLocation(){
+        //...
+    }
+}
